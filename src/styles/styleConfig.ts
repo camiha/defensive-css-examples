50ccec5a2b29e90ffd3d0acfa8478ad5a4db8f32
cyamy <hi.cyamy@gmail.com>
2023-04-24 23:52:17 +0900

    add style settings

---
+++ src/styles/styleConfig.ts
@@ -0,0 +1,14 @@
+export const colorConfig = {
+  primary: "#0A4D68",
+  secondary: "#088395",
+  tertiary: "#05BFDB",
+  quaternary: "#00FFCA",
+  dark: "#191414",
+  light: "#eeeeee",
+} as const;
+
+export const marginConfig = {
+  sm: 8,
+  md: 16,
+  lg: 32,
+} as const;
